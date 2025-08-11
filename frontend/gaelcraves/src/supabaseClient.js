a4b75cc91acb80432bf6c75e6e2eb37fea47dc28
aleguzmancs9 <aleguzmancs9@gmail.com>
2025-08-10 21:21:32 -0700

    Use environment variables for Supabase config
    Replaces hardcoded Supabase URL and anon key with environment variables.

--- frontend/gaelcraves/src/supabaseClient.js
+++ frontend/gaelcraves/src/supabaseClient.js
@@ -1,7 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dklnsndxjdqvfejbuoxl.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbG5zbmR4amRxdmZlamJ1b3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3OTI3ODAsImV4cCI6MjA3MDM2ODc4MH0.haPNUognHiRXDPTINII1co84I7omu6e5Aia-s6rl8wM'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
